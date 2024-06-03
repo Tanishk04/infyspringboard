437ecaf4d00526c77c215b5e5e75fd08c9b12ee7
Tanishk04 <tr15042001@gmail.com>
2024-06-04 00:52:50 +0530

    updated deployment settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j(@sbre8hebeag-offa&j@21vmrqv17mde3hg=5r+ywl7@9xfp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -130,3 +130,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,'testapp', 'static')]
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
