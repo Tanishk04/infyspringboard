27fdd046c11dcb033a274db677268f6839afc027
Tanishk04 <tr15042001@gmail.com>
2024-06-04 23:53:59 +0530

    final commit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j(@sbre8hebeag-offa&j@21vmrqv17mde3hg=5r+ywl7@9xfp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -122,6 +122,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+STATIC_ROOT = "/home/ubuntu/infyspringboard"
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'testapp', 'static')]
 
